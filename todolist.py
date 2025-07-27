ca68d059d9755f6062d8c6bb89d91221be33011c
Aman-1-1 <amanregmi10@gmail.com>
2025-07-27 17:53:37 +0545

    Completed TODO list

--- todolist.py
+++ todolist.py
@@ -4,16 +4,27 @@ def add(data):
         todo.insert(0,data)
     else:
         todo.append(data)
+    return main()
 def remove(data):
     todo.remove(data)
-print ("Enter the action to be Performed")
-print ("if u need to create new list just use Add")
-tasks =input ("1.Add  \n 2.Remove \n 3.View \n 4.Exit \n")
-task=tasks.lower()
-if tasks=="1" or tasks=="add" :
-    data=input ("Enter the task to be added :")
-    add (data)
-if task=="2"or task=="remove":
-    data=input ("Enter the task to be added :")
-    remove(data)
-if task=="3" or 
+    return main()
+def view():
+    for todoes in todo:
+        print (todoes)
+    return main()
+def main():
+    print ("Enter the action to be Performed")
+    print ("if u need to create new list just use Add")
+    tasks =input ("1.Add  \n 2.Remove \n 3.View \n 4.Exit \n")
+    task=tasks.lower()
+    if task=="1" or task=="add" :
+        data=input ("Enter the task to be added :")
+        add (data)
+    if tasks=="2"or task=="remove":
+        data=input ("Enter the task to be added :")
+        remove(data)
+    if task=="3" or task=="view":
+        view()
+    if task=="4" or task=="exit":
+        exit()
+main()
